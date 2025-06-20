97d5ca258751768915fd5975b379d6c269901838
anton3063 <anthonymayne@hotmail.co.uk>
2025-06-20 10:23:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"message": "Rentonomic backend is running!"}
+
+@app.get("/listings")
+def get_listings():
+    return {
+        "listings": [
+            {"id": 1, "title": "Electric Drill", "price_per_day": 5},
+            {"id": 2, "title": "Projector", "price_per_day": 10},
+            {"id": 3, "title": "Camping Tent", "price_per_day": 7}
+        ]
+    }
